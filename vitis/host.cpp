3b123560b9b2b1c1fc50b5ff72078b1845a1673c
narangsubeg2706 <snarang41@gatech.edu>
2025-05-02 13:51:33 -0400

    Update host.cpp

--- vitis/host.cpp
+++ vitis/host.cpp
@@ -1,84 +1,62 @@
 #include "header.h"
-
-// const bool IS_INPUT_LOCAL = false;
-
 using namespace std;
 
-// void read_input_file(const char *filename, fired_pixel file_line_array[])
-// {
-//     FILE *file = fopen(filename, "r");
-//     if (!file)
-//     {
-//         perror("Failed to open file");
-//         exit(1);
-//     }
-// }
-
-int main (int argc, char *argv[]) {
-    // // conditional addition of "../data/" to start of filename
-    // std::string redirect = "../data/";
-
-  
-    std::cout << "HOST: entered main(), argc=" << argc << "\n";
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <input_test_file.txt>\n";
-        return 1;
-    }
+int main(int argc, char *argv[]) {
+    // if the cosim harness gives you a filename, use it;
+    // otherwise fall back to a hard-coded test vector in the cosim working directory:
+    const char* fname = (argc > 1)
+        ? argv[1]
+        : "input_test_file.txt";   // put your vectors here
 
     // 1) Open input file
-    std::ifstream infile(argv[1]);
+    ifstream infile(fname);
     if (!infile) {
-        std::cerr << "Error: cannot open " << argv[1] << "\n";
+        cerr << "Error: cannot open " << fname << "\n";
         return 1;
     }
 
     // 2) Read & parse lines into a vector
-    std::vector<fired_pixel> inputs;
-    std::string line;
-    while (std::getline(infile, line)) {
+    vector<fired_pixel> inputs;
+    string line;
+    while (getline(infile, line)) {
         if (line.empty()) continue;
-
-        std::istringstream iss(line);
-        char flag;           // '0' or '1'
-        std::string payload; // e.g. "68A98" or "09F/0EB"
+        istringstream iss(line);
+        char flag;
+        string payload;
         if (!(iss >> flag >> payload)) {
-            std::cerr << "Skipping malformed line: " << line << "\n";
+            cerr << "Skipping malformed line: " << line << "\n";
             continue;
         }
-
         fired_pixel fp;
-        fp.is_end = 0;        // always zero for file
+        fp.is_end = 0;
         if (flag == '1') {
-            // new event → ID
             fp.is_new_event = 1;
-            fp.ID = (ID_t)std::stoul(payload, nullptr, 16);
+            fp.ID = (ID_t)stoul(payload, nullptr, 16);
         } else {
-            // coordinate line → col/row
             fp.is_new_event = 0;
             auto slash = payload.find('/');
-            std::string col_s = payload.substr(0, slash);
-            std::string row_s = payload.substr(slash + 1);
-            fp.coords.col = (col_idx_t)std::stoul(col_s, nullptr, 16);
-            fp.coords.row = (row_idx_t)std::stoul(row_s, nullptr, 16);
+            fp.coords.col = (col_idx_t)stoul(payload.substr(0, slash), nullptr, 16);
+            fp.coords.row = (row_idx_t)stoul(payload.substr(slash+1), nullptr, 16);
         }
         inputs.push_back(fp);
     }
 
-    // 3) Number of lines
-    unsigned int num_lines = inputs.size();
-    std::cout << "Read " << num_lines << " fired_pixel entries\n";
-    int max_cluster_count = 1000;
+    // 3) Report how many we got
+    unsigned num_lines = inputs.size();
+    cout << "Read " << num_lines << " fired_pixel entries\n";
 
-    // 4) Allocate output buffer (size <= num_lines)
-    std::vector<cluster> clusters(max_cluster_count);
+    // 4) Prepare output buffer
+    constexpr int max_clusters = 100;
+    vector<cluster> clusters(max_clusters);
 
-    // 5) Call your HLS kernel
+    // 5) Call your kernel
     HLS_kernel_columnar_cluster(
-        inputs.data(), // TBD: Vitis may or may not like this way of making an array
-        num_lines,
-        clusters.data(),
-	max_cluster_count
+      inputs.data(),
+      num_lines,
+      clusters.data(),
+      max_clusters
     );
 
     return 0;
 }
+
